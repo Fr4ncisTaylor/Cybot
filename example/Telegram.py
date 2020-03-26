13a2d27a2851a0c988670e065449372ec87290a6
Francis Taylor <francistrapp2000@gmail.com>
2020-03-26 14:53:57 -0300

    Telegram example

--- example/Telegram.py
+++ example/Telegram.py
@@ -1,1 +1,13 @@
 
+from Cybot import telegram, whatsapp
+from Cybot.telegram import loop
+from pprint import pprint
+
+webhook = "Webhook_URL"
+token   = 'telegram_bot_token'
+bot     = telegram.start(token, webhook)
+
+def hanler(msg):
+	pprint(msg)
+  
+telegram.loop.run(handler, debug=True)
